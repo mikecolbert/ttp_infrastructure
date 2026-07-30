209cf74f275069e92564daad4b8ab848137895c8
Mike Colbert <michael-colbert@uiowa.edu>
2026-07-30 14:28:47 -0500

    update file path

--- scripts/update-source.sh
+++ scripts/update-source.sh
@@ -1,18 +1,23 @@
 # ################ Set up environment for shared vars #####################
 set -eu pipefail # Enable strict error handling
 # Script's working directory
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-RELATIVE_PATH="${SCRIPT_DIR}/../../.."
+# SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(cd "/cluster-src/scripts" && pwd)"
+# echo "SCRIPT_DIR ${SCRIPT_DIR}"
+# RELATIVE_PATH="${SCRIPT_DIR}/../../.."
+RELATIVE_PATH="${SCRIPT_DIR}/.."
+# echo "RELATIVE_PATH ${RELATIVE_PATH}"
 # Repo cloned here
 REPO_PATH=$(realpath "$RELATIVE_PATH")
+# echo "REPO_PATH ${REPO_PATH}"
 # ############### END Set up environment ##################################
 
 echo "Updating infrastructure code"
-cd ${REPO_PATH}/book/
+cd ${REPO_PATH}/
 git pull
 echo "infrastructure code done"
 
-echo "Updating HTMX sample app code"
+echo "Updating app code"
 cd ${REPO_PATH}/containers/core-app/ttp-docker/src/ttp-app
 git pull
 echo "Application code done"
